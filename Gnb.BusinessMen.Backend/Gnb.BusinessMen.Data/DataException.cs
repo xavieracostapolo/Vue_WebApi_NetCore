2ac8a95b1dd9f878b1b9101ba273a38d332357a6
Xavier Acosta <xavieracosta@ingenieros.com>
2019-03-07 19:59:48 -0500

    Inicio de la solucion BackEnd

---
+++ Gnb.BusinessMen.Backend/Gnb.BusinessMen.Data/DataException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Gnb.BusinessMen.Data
+{
+    /// <summary>
+    /// Class manager exceptions in business logic application
+    /// </summary>
+    [Serializable]
+    public class DataException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataException"/> class.
+        /// </summary>
+        public DataException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        public DataException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or 
+        /// a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        public DataException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataException"/> class.
+        /// </summary>
+        /// <param name="info">Serialization info.</param>
+        /// <param name="context">Streaming Context.</param>
+        protected DataException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
