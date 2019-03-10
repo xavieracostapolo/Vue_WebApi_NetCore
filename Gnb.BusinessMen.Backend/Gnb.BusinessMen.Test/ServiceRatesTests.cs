48d845a276d7923560241a81f724550062c64ae0
Xavier Acosta <xavieracosta@ingenieros.com>
2019-03-10 12:20:36 -0500

    Se agregan algunas pruebas unitarias del proyecto de servicio

---
+++ Gnb.BusinessMen.Backend/Gnb.BusinessMen.Test/ServiceRatesTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Gnb.BusinessMen.Data;
+using Gnb.BusinessMen.Model;
+using Gnb.BusinessMen.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Gnb.BusinessMen.Test
+{
+    /// <summary>
+    /// Service rates tests.
+    /// </summary>
+    [TestClass]
+    public class ServiceRatesTests
+    {
+        /// <summary>
+        /// Gets all test.
+        /// </summary>
+        [TestMethod]
+        public void GetAllTest()
+        {
+            var moqDaoRates = new Mock<IDaoRates>();
+
+            moqDaoRates.Setup(r => r.GetAll()).Returns(new Collection<Rates>());
+
+            ServiceRates serviceRates = new ServiceRates(moqDaoRates.Object);
+
+            Assert.AreNotEqual(serviceRates.GetAll(), null);
+        }
+    }
+}
---
+++ Gnb.BusinessMen.Backend/Gnb.BusinessMen.Test/ServiceTransactionsTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Gnb.BusinessMen.Data;
+using Gnb.BusinessMen.Model;
+using Gnb.BusinessMen.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Gnb.BusinessMen.Test
+{
+    /// <summary>
+    /// Service transactions test.
+    /// </summary>
+    [TestClass]
+    public class ServiceTransactionsTest
+    {
+        /// <summary>
+        /// Gets all test.
+        /// </summary>
+        [TestMethod]
+        public void GetAllTest()
+        {
+            var moqDaoRates = new Mock<IDaoRates>();
+            var moqDaoTransactions = new Mock<IDaoTransactions>();
+
+            moqDaoTransactions.Setup(t => t.GetAll()).Returns(new Collection<Transactions>());
+
+            ServiceTransactions serviceTransactions = new ServiceTransactions(moqDaoTransactions.Object, moqDaoRates.Object);
+
+            Assert.AreNotEqual(serviceTransactions.GetAll(), null);
+        }
+
+        [TestMethod]
+        public void GetSumTotalBySku()
+        {
+            string sku = "T2006";
+            var moqDaoRates = new Mock<IDaoRates>();
+            var moqDaoTransactions = new Mock<IDaoTransactions>();
+
+            moqDaoTransactions.Setup(t => t.GetTotalBySku(sku)).Returns(new Collection<Transactions>());
+
+            ServiceTransactions serviceTransactions = new ServiceTransactions(moqDaoTransactions.Object, moqDaoRates.Object);
+
+            Assert.AreNotEqual(serviceTransactions.GetTotalBySku(sku), null);
+        }
+    }
+}
